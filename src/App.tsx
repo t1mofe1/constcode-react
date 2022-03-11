120f58ecd314a4f575903f604b996e451348ccaf
Timothy Myagi <timofei.magi@gmail.com>
2022-03-12 00:57:21 +0200

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from './components/Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<>
+			<Table
+				color="red"
+				rows={[
+					['item', 'item', 'item'],
+					['item', 'item', 'item'],
+					['item', 'item', 'item'],
+				]}
+			/>
+			<Table
+				color="green"
+				rows={[
+					['item', 'item', 'item'],
+					['item', 'item', 'item'],
+					['item', 'item', 'item'],
+				]}
+			/>
+			<Table
+				color="blue"
+				rows={[
+					['item', 'item', 'item'],
+					['item', 'item', 'item'],
+					['item', 'item', 'item'],
+				]}
+			/>
+		</>
+	);
 }
-
-export default App;
